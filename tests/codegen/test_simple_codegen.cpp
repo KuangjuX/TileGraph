f8b917b2c6029d2ea47b821e394db7ef7b06d887
Kuangjux <kuangjux@outlook.com>
2023-11-23 18:51:38 +0800

    add simple cuda testcase.

--- tests/codegen/test_simple_codegen.cpp
+++ tests/codegen/test_simple_codegen.cpp
@@ -6,19 +6,27 @@ using namespace ::tilegraph::codegen;
 
 constexpr static const char *code = R"~(
 #include <cstdio>
-__global__ void kernel() {
-    printf("Hello World from GPU!\n");
+__global__ void kernel(float* a) {
+    a[threadIdx.x] += 1.0;
 }
 extern "C" {
-void launchKernel() {
-    kernel<<<1, 1>>>();
+void launchKernel(float* a) {
+    float* dev_a;
+    cudaMalloc(&dev_a, 100 * sizeof(float));
+    cudaMemcpy(dev_a, a, 100 * sizeof(float), cudaMemcpyHostToDevice);
+    kernel<<<1, 100>>>(dev_a);
     cudaDeviceSynchronize();
+    cudaMemcpy(a, dev_a, 100 * sizeof(float), cudaMemcpyDeviceToHost);
 }
 }
 )~";
 
-TEST(generator, CudaCodeRepo) {
+TEST(Codegen, simple_cuda) {
     CudaCompiler nvcc;
-    auto function = nvcc.compile("helloWorld", code, "launchKernel");
-    reinterpret_cast<void (*)()>(function)();
+    auto function = nvcc.compile("add", code, "launchKernel");
+
+    float *a = new float[100];
+    memset(a, 0, 100 * sizeof(float));
+    reinterpret_cast<void (*)(float *)>(function)(a);
+    EXPECT_EQ(a[0], 1.0);
 }
